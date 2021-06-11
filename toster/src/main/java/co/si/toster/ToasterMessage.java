8a28626ca10d60675af6d69640fb8eaa04572f09
Hell Corp <corporation.h3ll@gmail.com>
2021-06-11 19:54:49 +0530

    first
    asc

---
+++ toster/src/main/java/co/si/toster/ToasterMessage.java
@@ -0,0 +1,13 @@
+package co.si.toster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void toast(Context c, String message) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+    }
+}
